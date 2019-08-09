6d9833f77d07cb8930380e31e6388b904566f65f
devin-bielejec <devin.bieleje@gmail.com>
2019-08-09 11:27:45 -0400

    Header is complete

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,5 +8,22 @@
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .headerContainer component
+function createElement(element, className, content, parent){
+    item = document.createElement(element);
+    className != "" ? item.classList.add(className) : false;
+    content != "" ? item.textContent = content : false;
+    parent != "" ? parent.appendChild(item): false;
+    return item;
+}
 
-function Header() {}
+
+function Header() {
+    const divHeader = createElement("div", "header", "", "");
+    const spanDate = createElement("span", "date", "MARCH 28, 2019", divHeader);
+    const h1 = createElement("h1", "", "Lambda Times", divHeader);
+    const spanTemp = createElement("span", "temp", "98°", divHeader);
+    return divHeader;
+}
+
+const headerContainer = document.querySelector(".header-container");
+headerContainer.appendChild(Header());
